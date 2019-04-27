06a8f01d15c8be22042f014ff6415feae377c8b4
0noketa <0noketa@gmail.com>
2019-04-27 14:31:34 +0900

    2019-04-27

--- JsonInjector.cs
+++ JsonInjector.cs
@@ -1,9 +1,11 @@
 // simple translator
 /* language:
     lines starts with "//[]":
-      command that will be separated at every '/'.
+      command that is a list separated at every '/'.
       first element means function, others are parameters.
-     
+      commands do not read or modify any other command.
+      any command that reads files can not accept any file name includes special characters.
+
       commands:
         byKey:
           param0: 
@@ -11,11 +13,11 @@
           param1..(joined with '/'):
             regex
           usage:
-            generates file names in JSON.
+            generates file names in JSON object.
             regex should contains a group as key. and treats entire file names as elements.
           example:
             script:
-              //[]byKey/mydir/([a-z]+)_[0-9]+.txt
+              //[]byKey/mydir/([a-z]+)_[0-9]+\.txt
             files:
               mydir/
                 abc_0.txt
@@ -24,7 +26,112 @@
             output(not in this format):
               { "abc" : [ "abc_0.txt", "abc_2.txt" ]
               , "def" : [ "def_0.txt" ] } 
-    lines does not start with "//[]":
+        all:
+          param0: 
+            directory
+          param1..(joined with '/'):
+            regex (optional)
+          usage:
+            generates file names in JSON array.
+            regex is guard. if omitted, all files are accepted.
+          example:
+            script:
+              //[]all/mydir/[a-z]+\.txt
+            files:
+              mydir/
+                abc_0.txt
+                abc_1.bin
+                abc_2.txt
+                def_1.bin
+                def_4.txt
+            output:
+              [ "abc_0.txt"
+              , "abc_2.txt"
+              , "def_4.txt"
+              ]
+        (not implemented):
+        defineComment:
+          param0:
+            label
+          param1:
+            regex
+          usage:
+            defines line-comments.
+        undefComment:
+          param0:
+            label
+          usage:
+            defines line-comments.
+          example:
+            input:
+              //[]defineComment/div/\/.*
+              var x = (1
+              / 2
+              / 3
+              + 4
+              )
+              //[]undefComment/div
+              var y = (1
+              / 2
+              / 3
+              + 4
+              )
+            output:
+              var x = (1
+              + 4
+              )
+              var y = (1
+              / 2
+              / 3
+              + 4
+              )
+        beginScan:
+          param0:
+            label
+          param1:
+            regex with 1 group
+          param2:
+            integer. size of queue. (optional. default=256)
+          usage:
+            begins to scan input. group values to queue.
+        endScan:
+          param0:
+            label
+          usage:
+            ends scanning.
+        scanned:
+          param0:
+            label
+          usage:
+            dump scanned values as array.
+          example:
+            input:
+              //[]beginScan/enum/var\s+(TYPE_[A-Z_]+[A-Z_0-9]*)\s*\=\s*[0-9]+\s*\;
+              //[]beginScan/name/var\s+TYPE_([A-Z_]+[A-Z_0-9]*)\s*\=\s*[0-9]+\s*\;
+              var TYPE_INT = 0;
+              var TYPE_STR = 1;
+              var TYPE_ARRAY = 2;
+              //[]endScan/enum
+              //[]endScan/name
+              var typeNameByEnum =
+              //[]scanned/name
+              var stringifiedByEnum =
+              //[]scanned/enum
+            output:
+              var TYPE_INT = 0;
+              var TYPE_STR = 1;
+              var TYPE_ARRAY = 2;
+              var typeNameByEnum =
+              [ "INT"
+              , "STR"
+              , "ARRAY"
+              ]
+              var stringifiedByEnum =
+              [ "TYPE_INT"
+              , "TYPE_STR"
+              , "TYPE_ARRAY"
+              ]
+    other lines:
       just print.
  */
 
@@ -35,13 +142,17 @@ using System.Linq;
 using System.Text.RegularExpressions;
 
 
-class JsonInjector
+public class JsonInjector
 {
     public static void Main(string[] args)
     {
-        Translate(Console.In, Console.Out);
+        if (args.Length > 0)
+            Eval(args, Console.Out);
+        else
+            Translate(Console.In, Console.Out);
     }
-    static void Translate(TextReader input, TextWriter output)
+
+    public static void Translate(TextReader input, TextWriter output)
     {
         string s;
 
@@ -66,40 +177,110 @@ class JsonInjector
                 continue;
             }
 
-            var cmdElms = s.Substring(4).Split('/');
+            if (!Eval(s.Substring(4).Split('/'), output))
+                output.WriteLine("//][ unknown command: {0}", s);
+        }
+    }
 
-            if (cmdElms[0] == "byKeys" && cmdElms.Length == 3)
+    static bool Eval(string[] cmd, TextWriter output)
+    {
+        if (cmd[0] == "byKey" && cmd.Length >= 3)
+        {
+            var dirPath = cmd[1];
+            var regex = RegexFromSlicedPatterns(cmd.Skip(2));
+
+            DumpFileDic(output, dirPath, regex);
+
+            return true;
+        }
+
+        if (cmd[0] == "all" && cmd.Length >= 2)
+        {
+            var dirPath = cmd[1];
+            Regex regex = cmd.Length >= 3
+                ? RegexFromSlicedPatterns(cmd.Skip(2))
+                : null;
+
+            DumpFileList(output, dirPath, regex);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    static string CompleteRegexStr(string regexStr)
+    {
+        if (!regexStr.StartsWith("^")) regexStr = "^" + regexStr;
+        if (!regexStr.EndsWith("$")) regexStr = regexStr + "$";
+
+        return regexStr;
+    }
+
+    static Regex RegexFromSlicedPatterns(IEnumerable<string> src)
+    {
+        var regexStr = CompleteRegexStr(string.Join("/", src));
+
+        return new Regex(regexStr, RegexOptions.ECMAScript);
+    }
+
+    static List<string> ListFromDir(string dirPath, Regex regex = null)
+    {
+        var r = new List<string>();
+
+        dirPath = dirPath.Trim();
+
+        if (dirPath.Contains("\\"))
+        {
+            IEnumerable<string> r2 = r;
+            var ds = dirPath.Split('\\');
+
+            foreach (var d in ds)
+                if (d != "")
+                    r2 = r2.Concat(ListFromDir(d, regex));
+            
+            return r2.ToList();
+        }
+
+        if (new Regex("^(?:.*(?:\\:|\\;|\\,|\\s).*|\\~|\\.{2,})$").IsMatch(dirPath))
+            return r;
+
+        try
+        {
+            var d = new DirectoryInfo(dirPath);
+
+            foreach (var f in d.GetFiles())
             {
-                var dirPath = cmdElms[1];
-                var regexStr = string.Join("/", cmdElms.Skip(2));
+                var fileName = f.Name;
 
-                if (!regexStr.StartsWith("^")) regexStr = "^" + regexStr;
-                if (!regexStr.EndsWith("$")) regexStr = regexStr + "$";
+                if (regex != null)
+                {
+                    var match = regex.Match(fileName);
 
-                var regex = new Regex(regexStr, RegexOptions.ECMAScript);
-
-                Dump(output, dirPath, regex);
-            }
-            else
-            {
-                output.WriteLine("// unknown command: {0}", s);
+                    if (!match.Success) continue;
+                }
+                
+                r.Add(fileName);
             }
         }
+        catch (Exception _)
+        { }
+
+        return r;
     }
 
     static Dictionary<string, List<string>> DicFromDir(string dirPath, Regex regex)
     {
         var r = new Dictionary<string, List<string>>();
-        var d = new DirectoryInfo(dirPath);
+        var list = ListFromDir(dirPath);
 
-        foreach (var f in d.GetFiles())
+        foreach (var fileName in list)
         {
-            var fileName = f.Name;
             var match = regex.Match(fileName);
 
             if (!match.Success || match.Groups.Count == 1) continue;
 
-            var v = match.Groups[1].Value;
+            var v = FirstNotEmptyGroupValue(match.Groups);
 
             if (!r.ContainsKey(v))
                 r.Add(v, new List<string>());
@@ -110,10 +291,32 @@ class JsonInjector
         return r;
     }
 
-    static void Dump(TextWriter output, string dirPath, Regex regex)
+    // first filled group
+    static string FirstNotEmptyGroupValue(GroupCollection groups)
+    {
+        for (int i = 1; i < groups.Count; ++i)
+            if (groups[i].Value != "")
+                return groups[i].Value;
+        
+        return "";
+    }
+
+    static void DumpFileDic(TextWriter output, string dirPath, Regex regex)
     {
         var dic = DicFromDir(dirPath, regex);
 
+        Dump(output, dic);
+    }
+
+    static void DumpFileList(TextWriter output, string dirPath, Regex regex = null)
+    {
+        var list = ListFromDir(dirPath, regex);
+
+        Dump(output, list);
+    }
+
+    static void Dump(TextWriter output, Dictionary<string, List<string>> dic)
+    {
         Action act_colon = () => output.Write(", ");
 
         Action sep = () => { };
@@ -126,24 +329,31 @@ class JsonInjector
 
             output.WriteLine("\"{0}\" :", key);
 
-            Action sep2 = () => { };
-
-            output.Write("[ ");
-
-            foreach (var key2 in dic[key])
-            {
-                sep2();
-
-                output.WriteLine("\"{0}\"", key2);
-
-                sep2 = act_colon;
-            }
-
-            output.WriteLine("]");
+            Dump(output, dic[key]);
 
             sep = act_colon;
         }
 
         output.WriteLine("}");
+    }
+
+    static void Dump(TextWriter output, List<string> list)
+    {
+        Action act_colon = () => output.Write(", ");
+
+        Action sep = () => { };
+
+        output.Write("[ ");
+
+        foreach (var key in list)
+        {
+            sep();
+
+            output.WriteLine("\"{0}\"", key);
+
+            sep = act_colon;
+        }
+
+        output.WriteLine("]");
     }
 }
---
+++ SplitPager.cs
@@ -0,0 +1,255 @@
+// 分割のみ
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+
+public class SplitPager
+{
+    TextReader input;
+    TextWriter output;
+
+    public bool HasNext { get; set; } = true;
+
+    int offset = 0;
+    public int Offset
+    {
+        get => offset;
+        set
+        {
+            if (value > 0)
+                offset = value;
+        }
+    }
+
+    int displayWidth = 120;
+    public int DisplayWidth
+    { 
+        get => displayWidth;
+        set
+        {
+            if (UpdatePageWidth(value, Columns, Separator.Length))
+                displayWidth = value;
+        }
+    }
+
+    public int PageWidth { get; private set; } = 59;
+    public int PageHeight { get; set; } = 29;
+
+    int columns = 2;
+    public int Columns
+    {
+        get => columns;
+        set
+        {
+            if (UpdatePageWidth(DisplayWidth, value, Separator.Length))
+                columns = value;
+        }
+    }
+
+    int tabWidth = 4;
+    public int TabWidth
+    {
+        get => tabWidth;
+        set
+        {
+            if (value > 0 && value < PageWidth)
+                tabWidth = value;
+        }
+    }
+
+    string separator = "|";
+    public string Separator
+    {
+        get => separator;
+        set
+        {
+            if (UpdatePageWidth(DisplayWidth, Columns, value.Length))
+                separator = value;
+        }
+    }
+
+    bool UpdatePageWidth(int dispW, int cols, int sepW)
+    {
+        if (dispW < 1 || cols < 1 || sepW < 0) return false;
+        if (dispW < cols * (sepW + 1)) return false;
+
+        PageWidth = dispW / cols - sepW;
+
+        return true;
+    }
+
+    public SplitPager(TextReader input = null, TextWriter output = null)
+    {
+        if (input == null) input = Console.In;
+        if (output == null) output = Console.Out;
+
+        this.input = input;
+        this.output = output;
+    }
+
+    static int SrcHeightToDstHeight(int h, int cols)
+    {
+        return h / cols + (h % cols > 0 ? 1 : 0);
+    }
+
+    public string[] LoadPage()
+    {
+        string[] src;
+
+        if (PageHeight < 0)
+        {
+            HasNext = false;
+            src = input.ReadToEnd()
+                .Split('\n')
+                .Select(s =>
+                    s.Replace("\r", "")
+                        .Replace("\t", new String(' ', TabWidth)))
+                .ToArray();
+            PageHeight = SrcHeightToDstHeight(src.Length, Columns);
+        }
+        else
+            src = new string[PageHeight * Columns]
+                .Select(_ =>
+                {
+                    var s = input.ReadLine();
+
+                    if (s == null)
+                    {
+                        HasNext = false;
+
+                        return "";
+                    }
+                    else
+                        return s;
+                })
+                .ToArray();
+
+        return src;
+    }
+
+    static int RequiredCharColumns(string s)
+    {
+        int r = 0;
+
+        foreach (char c in s.ToArray())
+            if (c >= 256) r += 2;
+            else ++r;
+
+        return r;
+    }
+
+    static int MaxCharsIntoCharColumns(string s, int offset, int w)
+    {
+        int i;
+        int r = 0;
+        int len2 = s.Length > offset ? s.Length - offset : 0;
+
+        for (i = 0; i < len2; ++i)
+        {
+            if (s[offset + i] >= 256) r += 2;
+            else ++r;
+
+            if (r > w)
+                return i;
+        }
+
+        return i;
+    }
+
+    string Substring(string s, int startIndex = -1, int length = -1)
+    {
+        if (startIndex == -1) startIndex = Offset;
+        if (length == -1 || length > s.Length) length = s.Length;
+
+        if (startIndex >= s.Length)
+            return "";
+
+        if (startIndex + length > s.Length)
+            length = s.Length - startIndex;
+
+        return s.Substring(startIndex, length);
+    }
+
+    void DumpPage(string[] src) 
+    {
+        string f(string s) =>
+            Substring(s, Offset, MaxCharsIntoCharColumns(s, Offset, PageWidth));
+        string fill(string s) => 
+            s.Length >= PageWidth
+            ? s 
+            : s + new String(' ', PageWidth - RequiredCharColumns(s));
+
+        for (int row = 0; row < PageHeight; ++row)
+        {
+            string s = f(src[row]);
+            var s2 = fill(s);
+
+            for (int col = 1; col < Columns; ++col)
+            {
+                int idx = rows * col + row;
+
+                if (idx < src.Length) s2 += Separator + fill(f(src[idx]));
+            }
+
+            output.WriteLine("{0}", s2);
+        }
+    }
+
+    void DumpPage()
+    {
+        DumpPage(LoadPage());
+    }
+
+
+    public static void Main(string[] args)
+    {
+        var opts = new Dictionary<string, string>();
+
+        foreach (var arg in args)
+        {
+            if (!"/-".Contains(arg[0])) continue;
+
+            var kv = arg.Substring(1).Split('=');
+
+            if (kv.Length == 1)
+                kv = kv[0].Split(':');
+            
+            if (kv.Length == 0) continue;
+
+            var key = kv[0];
+            var val = kv.Length > 1 ? kv[1] : "";
+
+            opts.Add(key, val);
+        }
+
+        if (opts.ContainsKey("help"))
+        {
+            Console.WriteLine("SplitPager [opts] < input > output |more");
+            Console.WriteLine("outions: -help -cols=N -w=N -h=N -sep=S -tab=N -offset=N");
+
+            return;
+        }
+
+        var pager = new SplitPager(Console.In, Console.Out);
+
+        if (opts.ContainsKey("cols"))
+            pager.Columns = Int32.Parse(opts["cols"]);
+        if (opts.ContainsKey("w"))
+            pager.DisplayWidth = Int32.Parse(opts["w"]);
+        if (opts.ContainsKey("h"))
+            pager.PageHeight = Int32.Parse(opts["h"]);
+        if (opts.ContainsKey("sep"))
+            pager.Separator = opts["sep"];
+        if (opts.ContainsKey("tab"))
+            pager.TabWidth = Int32.Parse(opts["tab"]);
+        if (opts.ContainsKey("offset"))
+            pager.Offset = Int32.Parse(opts["offset"]);
+        
+        while (pager.HasNext)
+        {
+            pager.DumpPage();
+        }
+    }
+}
